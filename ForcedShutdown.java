aec2e1b6fc8c8544d9fb33b66a8ced060c484242
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-11-17 19:26:08 -0500

    more lil fixes

--- ForcedShutdown.java
+++ ForcedShutdown.java
@@ -1,14 +1,12 @@
-package com.perry;
+package dev.hause.perry;
 
 /**
- * @author perry.
+ * @author hausemasterissue, perry
  * Started 11/16/2021.
  */
-public
-class ForcedShutdown {
+public class ForcedShutdown {
 
-    public static
-    void main(String[] args) {
+    public static void main(String[] args) {
         try {
             // execute the shutdown system command
             Runtime.getRuntime().exec("shutdown -s");
