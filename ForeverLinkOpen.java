231ab1cc0e8fc0e04d4a797d8b848ed01a5383a0
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-11-17 19:26:56 -0500

    tad bit cleaner

--- ForeverLinkOpen.java
+++ ForeverLinkOpen.java
@@ -1,21 +1,19 @@
-package com.perry;
+package dev.hause.perry;
 
 import java.awt.*;
 import java.io.IOException;
 import java.net.URI;
 
 /**
- * @author perry.
+ * @author hausemasterissue, perry
  * Started 11/16/2021.
  */
-public
-class ForeverLinkOpen {
+public class ForeverLinkOpen {
 
-    public static
-    void main(String[] args) {
+    public static void main(String[] args) {
         try {
             // open a url forever
-            for (; ; ) {
+            for (;;) {
                 String url = "your url here";
                 Desktop.getDesktop().browse(URI.create(url));
             }
